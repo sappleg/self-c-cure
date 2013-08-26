743932f9ffd858098688fcfa03f6513b5321c345
Spencer Applegate <spencer.applegate3@gmail.com>
2013-08-25 23:41:24 -0400

    Initial commit

---
+++ models/user.js
@@ -0,0 +1,39 @@
+var Schema = mongoose.Schema
+  , ObjectId = Schema.ObjectId
+  , Validations = require('./validations.js')
+  , salt = 'mySaltyString'
+  , SHA2 = new (require('jshashes').SHA512)()
+
+function encodePassword( pass ){
+	if( typeof pass === 'string' && pass.length < 6 ) return ''
+	
+	return SHA2.b64_hmac(pass, salt )
+}
+
+var UserSchema = new Schema({
+    email       : {type: String, required: true, unique: true, trim: true, lowercase: true } 
+  , password    : {type: String, set: encodePassword, required: true }
+});
+
+UserSchema.statics.classicLogin = function(login, pass, cb) {
+	if( login && pass ){
+		mongoose.models.User
+			.where( 'email', login )
+			.where( 'password', encodePassword(pass) )
+	  	.findOne( cb )
+	} else {
+		// just to launch the standard error
+		var o = new this({password: '', email: login+'aaa'})
+		o.save(cb)
+	}
+}
+
+UserSchema.path('email').validate( Validations.uniqueFieldInsensitive('User', 'email' ), 'unique' )
+UserSchema.path('email').validate( Validations.emailFormat, 'format' )
+
+UserSchema.path('password').validate( Validations.cannotBeEmpty, 'password' )
+
+UserSchema.plugin( mongoose.availablePlugins.timestamper )
+
+mongoose.model('User', UserSchema)
+
