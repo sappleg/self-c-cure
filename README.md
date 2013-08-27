38a5787f138760d0e02c92ba604e56a14bdecb3f
Spencer Applegate <spencer.applegate3@gmail.com>
2013-08-26 23:51:32 -0400

    Added angular seed code

---
+++ README.md
@@ -0,0 +1,6 @@
+Self C-Cure
+===========
+
+This webapp code for a Fall 2013 PenApps hackathon application.
+
+[MIT License](http://opensource.org/licenses/MIT) (c) Thotpod 2013
---
+++ src/app/app.js
@@ -0,0 +1,34 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: spencer
+ * Date: 7/14/13
+ * Time: 2:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+'use strict';
+
+angular.module('app', [
+        'auth',
+        'blog',
+        'meta',
+        'nav'
+    ])
+
+    .config(['$routeProvider', '$httpProvider', function($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'app/meta/about/about.html',
+            controller: 'meta.AboutCtrl',
+            activeTab: 'about'
+        });
+        $routeProvider.otherwise({redirectTo: '/'});
+    }])
+
+    .filter('interpolate', ['version', function(version) {
+        return function(text) {
+            return String(text).replace(/\%VERSION\%/mg, version);
+        };
+    }])
+
+    .value('version', '0.0.1')
+    .value('_api', 'http://dev.maasive.net/SuperSpock/spencer');
