f4da925f30756b5d267058845d4fbc8cfe204957
Spencer Applegate <spencer.applegate3@gmail.com>
2013-09-04 23:34:22 -0400

    Added device printout

--- models/models_main.js
+++ models/models_main.js
@@ -3,19 +3,22 @@
  */
 
 GLOBAL.mongoose = require('mongoose');
-mongoose.set('debug', true)
-mongoose.availablePlugins = require('../lib/mongoose-plugins')
-var url = app.config.mongodb_url
+mongoose.set('debug', true);
+mongoose.availablePlugins = require('../lib/mongoose-plugins');
+var url = app.config.mongodb_url;
 
-require('./user.js')
-require('./device.js')
+require('./user.js');
+require('./device.js');
 
 app.models = mongoose.models
 
 mongoose.connect(url);
 
-mongoose.models.User.count({}, function (err, num) { 
+mongoose.models.User.count({}, function(err, num) {
     console.log('users:', num);
 });
+mongoose.models.Device.count({}, function(err, num) {
+    console.log('devices:', num);
+});
 
-module.exports = mongoose.models
+module.exports = mongoose.models;
