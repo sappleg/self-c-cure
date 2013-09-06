45d31c65cc925d42940360b322da559dae855a9e
Spencer Applegate <spencer.applegate3@gmail.com>
2013-09-06 17:17:27 -0400

    Added nodemailer

--- helpers/mailer.js
+++ helpers/mailer.js
@@ -8,5 +8,35 @@
 
 'use strict';
 
-var mailer = require('mailer');
+var nodemailer = require('nodemailer'),
+    email;
 
+// create reusable transport method (opens pool of SMTP connections)
+var smtpTransport = nodemailer.createTransport("SMTP",{
+    service: "Gmail",
+    auth: {
+        user: "self.c.cure@gmail.com",
+        pass: "PenAppsFall2013"
+    }
+});
+
+// setup e-mail data with unicode symbols
+var mailOptions = {
+    from: "self.c.cure@gmail.com", // sender address
+    to: "bar@blurdybloop.com, baz@blurdybloop.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world ✔", // plaintext body
+    html: "<b>Hello world ✔</b>" // html body
+}
+
+// send mail with defined transport object
+smtpTransport.sendMail(mailOptions, function(error, response){
+    if(error){
+        console.log(error);
+    }else{
+        console.log("Message sent: " + response.message);
+    }
+
+    // if you don't want to use this transport object anymore, uncomment following line
+    //smtpTransport.close(); // shut down the connection pool, no more messages
+});
