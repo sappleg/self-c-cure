be2ee5fb39f5c10529425cc327bf613cdb05afb3
Hunter Brennick <hunterbrennick@gmail.com>
2013-09-06 21:35:33 -0400

    Changed style on login page

--- src/app/login/login.js
+++ src/app/login/login.js
@@ -10,21 +10,75 @@
 
 angular.module('login', function () {})
 
-    .controller('LoginCtrl', ['$scope', '$location',
-        function($scope, $location) {
+    .controller('LoginCtrl', ['$scope', '$location', '$http',
+        function($scope, $location, $http) {
         $scope.swag = {
             createAccTxt: "Create Account",
             createAccBtn: "default"
         };
-
-
+        $scope.user = {
+            email: '',
+            password: ''
+        }
 
         $scope.login = function () {
-            //if auth
-                //GET data
-                    //set path
-            $location.path('/landing');
-        }
+
+            var path = 'http://localhost:8142/user/5229022c02993c2510000001/devices/',
+                body =
+                    {
+                        "name":"Window",
+                        "limit":"30",
+                        "ranges": [{
+                            "upper": "0000",
+                            "lower": "0100"
+                        }
+                        ],
+                        "armed":"true"
+                    };
+            $http.post(path, body).then(function (data) {
+                    console.log(data);
+
+                },
+                function (response) {
+                    console.log(response);
+                });
+
+
+
+
+
+
+            /*
+            var path ='http://localhost:8142/auth/login/',
+                body = {
+                    "email": $scope.user.email,
+                    "pass": $scope.user.password
+                };
+
+            console.log(body);
+            $http.post(path, body).then(function (data, response) {
+                //$location.path('/landing');
+                console.log(data);
+
+            },
+            function (response) {
+                console.log(response);
+//                $scope.user.password = "";
+            });
+            */
+
+            /*
+            var id ='5229022c02993c2510000001';
+            var path ='http://localhost:8142/user/5229022c02993c2510000001';
+
+            $http.get(path).then(function(data) {
+                console.log(data);
+            }, function() {
+
+            });
+            */
+
+        };
 
         $scope.createAccountFields = function () {
             $scope.createAccount = !$scope.createAccount;
@@ -42,5 +96,13 @@ angular.module('login', function () {})
         $scope.createAccount = function () {
             //POST valid .. note check if initPass==confirmPass
                 //on success --> $location.path('/landing');
+        }
+
+        var getUser = function () {
+            $http.get(path).then(function(data) {
+                console.log(data);
+            }, function() {
+
+            });
         }
     }]);
