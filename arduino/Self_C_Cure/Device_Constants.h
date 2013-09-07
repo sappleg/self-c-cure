9d2596c5af32b79fcd12b07420a00742e27ed6a2
Craig McCown <craigrmccown@seas.0924.apng.wireless-pennnet.upenn.edu>
2013-09-07 04:57:58 -0400

    updated device constants to point to thotpod server

--- arduino/Self_C_Cure/Device_Constants.h
+++ arduino/Self_C_Cure/Device_Constants.h
@@ -2,9 +2,9 @@
 #ifndef DEVICE_CONSTANTS
 #define DEVICE_CONSTANTS
 
-char deviceId[] = "/spencerisafag";
-char baseEndpoint[] = "http://192.168.43.61:8142";
-char openEndpoint[] = "/open";
-char closeEndpoint[] = "/closed";
+char deviceId[] = "spencerisafag/";
+char baseEndpoint[] = "http://self-c-cure.thotpod.com:8142/";
+char openEndpoint[] = "open/";
+char closeEndpoint[] = "closed/";
 
 #endif
